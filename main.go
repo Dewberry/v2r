7736f6cf411a552bbfe1c198496562e1d3b64488
Justin Paul <jpaul@dewberry.com>
2022-06-22 08:16:54 -0400

    add entrypoint

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	cleaner "app/features/cleaner"
 	idw "app/features/idw"
+	"app/tests"
 	"app/tools"
 	processing "app/tools/processing"
+	"flag"
 	"fmt"
 	"log"
 	"strings"
@@ -12,31 +14,39 @@ import (
 )
 
 func main() {
-	doIdw := false
-	doClean := true
+	var path string
+	flag.StringVar(&path, "f", "", "pathtotif")
+	flag.Parse()
 
+	doIdw := false
+	doClean := false
+	doTests := false
+	fmt.Println("hello", path)
 	if doIdw {
 		doIDW()
 	}
 	if doClean {
-		clean()
+		clean(path)
+	}
+	if doTests {
+		tests.TestSuite()
 	}
 }
 
-func clean() {
+func clean(filepath string) {
 	start := time.Now()
 
 	//variables to change
 	adjType := 8 // 4 or 8
-	filepath := "data/cleaner/clipped_wet_dry.tif"
+	// filepath := "data/cleaner/WD_2100_MHHW.tif" // passed through
 	// toleranceIsland := 40000.0 // standard tolerance
 	// toleranceVoid := 22500.0   // standard tolerance
 	// useChunk := false
-	toleranceIsland := 400.0 // test smaller datasets
-	toleranceVoid := 225.0   // test smaller datasets
+	toleranceIsland := 40000.0 // test smaller datasets
+	toleranceVoid := 22500.0   // test smaller datasets
 	useChunk := true
-	chunkx := 150
-	chunky := 100
+	chunkx := 256 * 5
+	chunky := 256 * 5
 	chunkChannelSize := 20
 	//variables to change
 
@@ -66,7 +76,7 @@ func doIDW() {
 	start := time.Now()
 
 	//variables to change
-	useChunking := true
+	useChunking := false
 	chunkR := 200
 	chunkC := 250
 	powStep := .5
@@ -75,8 +85,8 @@ func doIDW() {
 	stepX := 100.0
 	stepY := 100.0
 	epsg := 2284
-	inputQuery := "SELECT elevation, ST_X(geom), ST_Y(geom) FROM sandbox.location_1;"
-	outfileFolder := "data/idw/"
+	// inputQuery := "SELECT elevation, ST_X(geom), ST_Y(geom) FROM sandbox.location_1;"
+	outfileFolder := "data/small/"
 	//variables to change
 
 	chunkString := ""
@@ -85,25 +95,25 @@ func doIDW() {
 	}
 
 	// From txt file
-	// inputFile := "data/small/nb2.txt"
-	// listPoints, xInfo, yInfo, err := processing.ReadData(inputFile)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// From txt file
-
-	//From db
-	db := processing.DBInit()
-
-	err := processing.PingWithTimeout(db)
-	if err != nil {
-		fmt.Println("Connected to database?", err)
-	}
-
-	listPoints, xInfo, yInfo, err := processing.ReadPGData(db, inputQuery, stepX, stepY)
+	inputFile := "data/small/idw_in.txt"
+	listPoints, xInfo, yInfo, err := processing.ReadData(inputFile)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// From txt file
+
+	//From db
+	// db := processing.DBInit()
+
+	// err := processing.PingWithTimeout(db)
+	// if err != nil {
+	// 	fmt.Println("Connected to database?", err)
+	// }
+
+	// listPoints, xInfo, yInfo, err := processing.ReadPGData(db, inputQuery, stepX, stepY)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	//From db
 
 	data := tools.MakeCoordSpace(&listPoints, xInfo, yInfo)
