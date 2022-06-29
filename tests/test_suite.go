8f54b349319f39bcc1ec466ec72b7d92b5ee2a4c
Justin Paul <jpaul@dewberry.com>
2022-06-29 17:05:47 -0400

    add geopackage read test

--- tests/test_suite.go
+++ tests/test_suite.go
@@ -5,7 +5,9 @@ import bunyan "github.com/Dewberry/paul-bunyan"
 func TestSuite() {
 
 	passedIDW := testIDW()
+	passedGPKGRead := testGPKGRead()
 	passedCleaner := testCleaner()
 	bunyan.Infof("Passed All IDW Tests?      %v", passedIDW)
+	bunyan.Infof("GPKG read correct?         %v", passedGPKGRead)
 	bunyan.Infof("Passed All Cleaner Tests?  %v", passedCleaner)
 }
