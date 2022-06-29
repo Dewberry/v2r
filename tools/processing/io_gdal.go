6201a239e3d24e7fc72803b1b8d458d509d5735d
Justin Paul <jpaul@dewberry.com>
2022-06-29 17:05:09 -0400

    add validation for field and layer of gpkg

--- tools/processing/io_gdal.go
+++ tools/processing/io_gdal.go
@@ -1,9 +1,10 @@
 package processing
 
 import (
-	"app/tools"
 	"fmt"
 	"strings"
+
+	"github.com/dewberry/v2r/tools"
 
 	bunyan "github.com/Dewberry/paul-bunyan"
 	"github.com/dewberry/gdal"
@@ -35,13 +36,12 @@ func WriteAscii(unwrappedMatrix interface{}, GDINFO GDalInfo, filename string, o
 func WriteGDAL(unwrappedMatrix interface{}, GDINFO GDalInfo, filename string, driver string, offsets tools.OrderedPair, totalSize tools.OrderedPair, bufferSize tools.OrderedPair, create bool) error {
 	var dataset gdal.Dataset
 	if create {
-		// fmt.Println("Creating Raster")
+		bunyan.Debug("Creating Raster")
 		driver, err := gdal.GetDriverByName(driver)
 		if err != nil {
 			bunyan.Fatal(err)
-			return err
 		}
-		// fmt.Printf("Creating dataset\n")
+		bunyan.Debug("Creating Dataset")
 		dataset = driver.Create(filename, totalSize.C, totalSize.R, 1, GDINFO.GDalDataType, []string{"BIGTIFF=YES"})
 
 		defer dataset.Close()
@@ -50,12 +50,10 @@ func WriteGDAL(unwrappedMatrix interface{}, GDINFO GDalInfo, filename string, dr
 		dataset.SetGeoTransform([6]float64{GDINFO.XMin, GDINFO.XCell, 0, GDINFO.YMin, 0, GDINFO.YCell})
 
 	} else {
-		// fmt.Println("Updating Raster")
 		var err error
 		dataset, err = gdal.Open(filename, gdal.Update)
 		if err != nil {
 			bunyan.Fatal(err)
-			return err
 		}
 
 		defer dataset.Close()
@@ -69,7 +67,7 @@ func WriteGDAL(unwrappedMatrix interface{}, GDINFO GDalInfo, filename string, dr
 func ReadGDAL(filepath string, offsets tools.OrderedPair, size tools.OrderedPair, entireFile bool) ([]byte, GDalInfo, tools.OrderedPair, error) {
 	DS, err := gdal.Open(filepath, gdal.ReadOnly)
 	if err != nil {
-		return []byte{}, GDalInfo{}, tools.OrderedPair{}, err
+		bunyan.Fatal(err)
 	}
 	if entireFile {
 		size = tools.MakePair(DS.RasterYSize(), DS.RasterXSize())
@@ -84,7 +82,7 @@ func ReadGDAL(filepath string, offsets tools.OrderedPair, size tools.OrderedPair
 	data := make([]byte, size.C*size.R)
 	err = band.IO(gdal.Read, offsets.C, offsets.R, size.C, size.R, data, size.C, size.R, 0, 0)
 	if err != nil {
-		return []byte{}, gdReturn, size, err
+		bunyan.Fatal(err)
 	}
 
 	return data, gdReturn, size, nil
@@ -93,7 +91,7 @@ func ReadGDAL(filepath string, offsets tools.OrderedPair, size tools.OrderedPair
 func GetInfoGDAL(filepath string) (GDalInfo, tools.OrderedPair, error) {
 	DS, err := gdal.Open(filepath, gdal.ReadOnly)
 	if err != nil {
-		return GDalInfo{}, tools.OrderedPair{}, err
+		bunyan.Fatal(err)
 	}
 
 	numCols := DS.RasterXSize()
@@ -123,18 +121,46 @@ func TransferType(src string, dst string, outputType string) {
 	}
 }
 
-func getGPKGPoints(filepath string, layer string, field string) ([]tools.Point, gdal.SpatialReference) {
+func validGPKGLayer(filepath string, layer string) {
 	ds := gdal.OpenDataSource(filepath, int(gdal.ReadOnly))
 	defer ds.Destroy()
 
-	bunyan.Debug(filepath, layer, field)
+	allLayers := make([]string, 0, ds.LayerCount())
+	for i := 0; i < ds.LayerCount(); i++ {
+		if ds.LayerByIndex(i).Name() == layer {
+			return
+		}
+		allLayers = append(allLayers, ds.LayerByIndex(i).Name())
+	}
+	bunyan.Fatalf("Invalid Layer: %v  | %v Possible Layers: %v", layer, len(allLayers), allLayers)
+}
+
+func getFieldIndex(fieldDef gdal.FeatureDefinition, field string) int {
+	ind := fieldDef.FieldIndex(field)
+	if ind != -1 {
+		return ind
+	}
+	allFields := make([]string, 0, fieldDef.FieldCount())
+	for i := 0; i < fieldDef.FieldCount(); i++ {
+		allFields = append(allFields, fieldDef.FieldDefinition(i).Name())
+	}
+	bunyan.Fatalf("Invalid Field: %v  | %v Possible Fields: %v", field, len(allFields), allFields)
+	return -1
+}
+
+func getGPKGPoints(filepath string, layer string, field string) ([]tools.Point, gdal.SpatialReference) {
+	bunyan.Debugf("%s %s %s", filepath, layer, field)
+	validGPKGLayer(filepath, layer)
+
+	ds := gdal.OpenDataSource(filepath, int(gdal.ReadOnly))
+	defer ds.Destroy()
+
 	l := ds.LayerByName(layer)
 	bunyan.Debug((l.Name()))
 
 	fieldDef := l.Definition()
-	bunyan.Debugf("field def: %v\tcount: %v\n", fieldDef.Name(), fieldDef.FieldCount())
 
-	fieldIndex := fieldDef.FieldIndex(field)
+	fieldIndex := getFieldIndex(fieldDef, field)
 	bunyan.Debug("field index", fieldIndex)
 
 	count, _ := l.FeatureCount(true)
